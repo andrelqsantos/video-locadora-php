b3784b6cc60f4326a02a3b15e983fe0213ebd9dd
André Luiz Queiroz dos Santos <andrelqsantos@gmail.com>
2017-12-11 15:36:57 -0300

    Primeiros passos do sistema

--- index.php
+++ index.php
@@ -4,37 +4,64 @@
 	<meta charset="UTF-8">
   <title>Janela Principal</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Latest compiled and minified CSS -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-<div class="btn-group">
-  <button type="button" class="btn btn-warning">Cliente</button>
-  <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-  <span class="caret"></span>
-  <span class="sr-only">Toggle Dropdown</span>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Video-Locadora</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
   </button>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="interfaces/JanelaCadastroCliente.php">Cadastrar</a></li>
-
-  </ul>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cliente
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="../estudos/interfaces/JanelaCadastroCliente.php">Cadastrar</a>
+        </div>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Filme
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Cadastrar</a>
+          <a class="dropdown-item" href="#">Reservar</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="width: 50%; height: 300px;" class="d-block w-100" src="https://www.google.com.br/about/img/social/generic-feed.svg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img style="width: 20%; height: 300px;" class="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img style="width: 50%; height: 300px;" class="d-block w-100" src="http://lh3.googleusercontent.com/jN9tX6dCJ6_XL9E4K1KCO2Tuwe9_rYUbwv723eu6XGI0PWGLcPs0259VscOu249PPKKcU5AOXrq6JnleEaoK6K_JvZ2PY9lw3pMApzOpTQ=s660" alt="Third slide">
+    </div>
+  </div>
+  <a style="background: #000; width: 5%;" class="carousel-control-prev" href="" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a style="background: #000; width: 5%;" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-<div class="btn-group">
-	<button type="button" class="btn btn-danger">Filme</button>
-  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-  <span class="caret"></span>
-  <span class="sr-only">Toggle Dropdown</span>
-  </button>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="#">Cadastrar</a></li>
-    <li><a href="#">Reservar</a></li>
-  </ul>
-</div>
-
-
-
-
 </body>
 </html>
--- interfaces/JanelaCadastroCliente.php
+++ interfaces/JanelaCadastroCliente.php
@@ -1,36 +1,71 @@
 <html>
-
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Video Locadora</title>
-  </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-  <body>
-    <pre>
-      <form class="JanelaCadastroCliente" method="POST" action= "inserir.php">
-            <p>CLIENTES CADASTRADOS<select name="visão">
-                    <option value="-"></option>
-                </select>
-              </p>
-CPF: <input type="text" name="CPF">
+    <link rel="stylesheet" type="text/css" href="JanelaCadastroCliente.css">
+</head>
+<body>
+  <!-- As a link -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="../index.php">Video-Locadora</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cliente
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Cadastrar</a>
+        </div>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Filme
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Cadastrar</a>
+          <a class="dropdown-item" href="#">Reservar</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+  <pre>
+    <div class="container">
+    <form class="JanelaCadastroCliente" method="POST" action= "inserir.php">
+      <p id="cliente">CLIENTES CADASTRADOS<select id="sele" name="visão">
+        <option value="-"></option>
+      </select>
+      </p>
+    <p id="CPF">CPF:</p> 
+    <input id="cpf" type="text" name="CPF" placeholder="CPF do cliente" class="form-control" required>
 
-NOME:<input type="text" name="nome">
+    <p id="nome">NOME:</p>
+    <input id="cpf" type="text" name="nome" placeholder="Nome do cliente" class="form-control" required>
 
-ENDEREÇO:<input type="text" name="endereço">
+    <p id="nome">ENDEREÇO:</p>
+    <input id="cpf" type="text" name="endereco" placeholder="Endereço do cliente" class="form-control" required>
 
-TELEFONE:<input type="text" name="telefone"><br><br>
+    <p id="nome">TELEFONE:</p>
+    <input id="cpf" type="text" name="telefone" placeholder="Telefone do cliente" class="form-control" required><br><br>
+  </div>
 </pre>
-        <input type="submit" name="inserir" value="inserir">
-        <input type="submit" name="consultar" value="consultar">
-        <input type="submit" name="alterar" value="alterar">
-        <input type="submit" name="remover" value="remover">
-        <input type="reset" value="limpar">
+<div id="botoes">
+        <input type="submit" name="inserir" value="inserir" class="btn btn-outline-success" style="cursor: pointer;">
+        <input type="submit" name="consultar" value="consultar" class="btn btn-outline-info" style="cursor: pointer;">
+        <input type="submit" name="alterar" value="alterar" class="btn btn-outline-primary" style="cursor: pointer;">
+        <input type="submit" name="remover" value="remover" class="btn btn-outline-danger" style="cursor: pointer;">
+        <input type="reset" value="limpar" class="btn btn-outline-warning" style="cursor: pointer;">
+      </div>
       </form>
 
   </body>
-<pre>
-
-<pre>
-
-
 </html>
